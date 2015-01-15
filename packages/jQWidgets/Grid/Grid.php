104268574ef989b1a19b2f51e4aae6fb0fec4c33
Camilo Bravo <hola@cambraca.com>
2015-01-15 00:36:06 -0500

    jQWidgets component

---
+++ packages/jQWidgets/Grid/Grid.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace jQWidgets;
+
+class Grid extends \Common\Grid {
+  static $dependencies = ['jQWidgets\\jQWidgets'];
+
+  //TODO: methods to render the table using jQWidgets
+}
---
+++ packages/jQWidgets/jQWidgets/jQWidgets.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace jQWidgets;
+
+class jQWidgets extends \Core\Component {
+  static $dependencies = ['jQuery\jQuery'];
+
+  const LIBRARY_PATH = 'jqwidgets-ver3.6.0/jqwidgets';
+}
